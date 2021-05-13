b23d330f14f4c765b00ef162696012818ab96d0e
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-05-13 14:45:04 +0900

    임의의 수 만들기(random)

---
+++ 보드게임.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(void)
+{
+	// 칸의 최대갯수
+	int max_length = 61;
+
+	// 주사위
+	int dice;
+
+	srand(time(0));
+	rand();
+	dice = rand();
+	printf("%d\n", dice);
+}
