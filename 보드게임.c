7189c42f80622a874a73761856062335069fd144
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-05-13 15:10:41 +0900

    주사위 무한으로 즐겨요 명륜진사주사위
    while(1) 로 무한반복

--- 보드게임.c
+++ 보드게임.c
@@ -7,11 +7,24 @@ void main(void)
 	// 칸의 최대갯수
 	int max_length = 61;
 
+	// 플레이어의 위치
+	int position = 0;
+
 	// 주사위
 	int dice;
 
 	srand(time(0));
 	rand();
-	dice = rand() % 6 + 1;
-	printf("%d\n", dice);
+
+	printf("현재위치 : %d\n\n", position);
+	getchar();
+
+	while (1)
+	{
+		// 주사위 설정 (1부터 6까지)
+		dice = rand() % 6 + 1;
+		position += dice;
+		printf("현재위치 : %d\n\n", position);
+		getchar();
+	}
 }
