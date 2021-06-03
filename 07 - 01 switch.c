510e0acccea01a5823a8affe01e583c5b3a7593d
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-06-03 13:47:47 +0900

    [문법설명] switch 바꾸기 (break로 의도한 코드만 실행)

--- 07 - 01 switch.c
+++ 07 - 01 switch.c
@@ -7,12 +7,14 @@ void main(void)
 	switch (a)
 	{
 	case 1:
-		printf("Ä­ÃÝ ¸¶½Ë´Ù\n");
-	
+		printf("Ä­ÃÝ ¸¶½Ë´Ù\n"); 
+		break;
 	case 2: 
 		printf("¾Æ¸óµå »©»©·Îµµ ¸ÀÀÖÀ½\n");
+		break;
 
 	default: 
 		printf("±Ùµ¥ ¶±ººÀÌ ¸Ô°í ½Í¾î\n");
+		break;
 	}
 }
