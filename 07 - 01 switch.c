a4e733c6c3e974c7247a44ce607e1c1026f99f5a
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-06-03 13:38:42 +0900

    [문법설명] switch 바꾸기 전

---
+++ 07 - 01 switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int a = 3;
+
+	if (a == 1)
+	{
+		printf("Ä­ÃÝ ¸¶½Ë´Ù");
+	}
+	else if (a == 2)
+	{
+		printf("¾Æ¸óµå »©»©·Îµµ ¸ÀÀÖÀ½");
+	}
+	else
+	{
+		printf("±Ùµ¥ ¶±ººÀÌ ¸Ô°í ½Í¾î");
+	}
+}
