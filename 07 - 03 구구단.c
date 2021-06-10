5c78da680fd2788a53830a3355be24ca4e9f6836
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-06-10 14:54:50 +0900

    [문법설명] for문을 while문으로 변경

--- 07 - 03 구구단.c
+++ 07 - 03 구구단.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 void main (void)
-{
-	int dan = 3;
-	for (int i = 1; i <= 9; i++)
+{	
+	int dan = 2;
+	int i = 1;
+	while (i <= 9)
 	{
-		printf("%d X %d = %d\n", dan, i, dan * i);
+		printf("%d X %d = %d\n", dan, i, dan*i);
+		i++;
 	}
-
-	/*
-	printf("2 X 1 = %d\n", 2*1);
-	printf("2 X 2 = %d\n", 2*2);
-	printf("2 X 3 = %d\n", 2*3);
-	printf("2 X 4 = %d\n", 2*4);
-	printf("2 X 5 = %d\n", 2*5);
-	printf("2 X 6 = %d\n", 2*6);
-	printf("2 X 7 = %d\n", 2*7);
-	printf("2 X 8 = %d\n", 2*8);
-	printf("2 X 9 = %d\n", 2*9);
-	*/
 }
