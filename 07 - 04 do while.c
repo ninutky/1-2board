1ca48f4dac344a91a17a0267e1ffc49f339476d8
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-06-18 15:05:54 +0900

    [문법 설명]do while 한거

--- 07 - 04 do while.c
+++ 07 - 04 do while.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 void main(void)
 {
-	// 한 번도 출력되지 않는다.
-	while ()0
+	// 일단 한번은 출력하고 본다.
+	do {
+		printf("11111\n");
+	} while (0);
+
+	// 한번도 출력되지 않는다.
+	while (0)
 	{
-		printf("출력해줘\n");
+		printf("22222\n");
 	}
 }
