9ed3a0a538e0e69ede5fe0fca60de7d1f54be729
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-08-26 14:38:11 +0900

    Add files via upload

---
+++ 문법설명/08 - 04 함수의 선언과 정의.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+// 함수의 선언 (declaration)
+int sum(int a, int b);
+int sum2(int a, int b);
+
+void main(void)
+{
+	int c = 3;
+	int d = 4;
+	int aaaaa = sum(c, d);	// 호출
+	printf("%d", aaaaa);
+}
+
+// 함수의 정의 (definition)
+int sum(int a, int b)
+{
+	return sum2(a, b);
+}
+
+int sum2(int a, int b)
+{
+	return a * b;
+}
