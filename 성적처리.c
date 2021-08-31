46202bf98bfd3e151f0c04e23487f5c888461ecb
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-08-31 15:09:47 +0900

    함수화(refactoring)

--- 성적처리.c
+++ 성적처리.c
@@ -1,48 +1,37 @@
 ﻿#include <stdio.h>
 
+// 입력받은 점수(score)에 대한 등급을 출력
+void cal_grade(int score) 
+{
+	if (90 <= score && score <= 100)
+		printf("A입니다. \n");
+	else if (80 <= score)
+		printf("B입니다. \n");
+	else if (70 <= score)
+		printf("C입니다. \n");
+	else if (60 <= score)
+		printf("D입니다. \n");
+	else
+		printf("E입니다. \n");
+}
+
 void main(void)
 {
 	int C_score;
 	printf("C점수를 입력하세요 : ");
 	scanf("%d", &C_score);
-	if (90 <= C_score && C_score <= 100)
-		printf("A입니다. \n");
-	else if (80 <= C_score)
-		printf("B입니다. \n");
-	else if (70 <= C_score)
-		printf("C입니다. \n");
-	else if (60 <= C_score)
-		printf("D입니다. \n");
-	else
-		printf("E입니다. \n");
+	
+	cal_grade(C_score);
 	{
 		int java_score;
 		printf("자바점수를 입력하세요 : ");
 		scanf("%d", &java_score);
-		if (90 <= java_score && java_score <= 100)
-			printf("A입니다. \n");
-		else if (80 <= java_score)
-			printf("B입니다. \n");
-		else if (70 <= java_score)
-			printf("C입니다. \n");
-		else if (60 <= java_score)
-			printf("D입니다. \n");
-		else
-			printf("E입니다. \n");
+		cal_grade(java_score);
 	}
 	{
 		int CA_score;
 		printf("컴구점수를 입력하세요 : ");
 		scanf("%d", &CA_score);
-		if (90 <= CA_score && CA_score <= 100)
-			printf("A입니다. \n");
-		else if (80 <= CA_score)
-			printf("B입니다. \n");
-		else if (70 <= CA_score)
-			printf("C입니다. \n");
-		else if (60 <= CA_score)
-			printf("D입니다. \n");
-		else
-			printf("E입니다. \n");
+		cal_grade(CA_score);
 	}
 }
