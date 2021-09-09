81a5b37d00911245f5c87fe13f1a10c56a0e82cf
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-09-09 14:31:33 +0900

    NULL문자를 이용하여 스마트하게

--- 문법설명/10 - 03 문자열02.c
+++ 문법설명/10 - 03 문자열02.c
@@ -4,7 +4,7 @@ void main(void)
 {
 	char str[12] = "Hello World";
 
-	// 문자열의 개수를 무식하게 세어서 11
-	for (int i=0; i<11; i++)
+	// 널문자('\0')가 아닐때까지 출력
+	for (int i=0; str[i]!='\0'; i++)
 		printf("%c", str[i]);
 }
