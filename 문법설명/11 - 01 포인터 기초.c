347e8d1fb23f472e26f0dda7064baf30a892fe0c
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-09-23 13:57:09 +0900

    포인터 변수도 메모리 공간에 할당되어야 한다

--- 문법설명/11 - 01 포인터 기초.c
+++ 문법설명/11 - 01 포인터 기초.c
@@ -6,7 +6,8 @@ void main(void)
 	// ptr이란 포인터 변수를 만들어서 num을 가리킴
 	int* ptr = &num;
 	printf("num안에 들어있는 값 %d\n", num);
-	printf("num의 메모리 주소값 %p\n", &num);	// %p = 16진수
+	printf("num의 메모리 주소값 %p\n", &num);	// %p = 16진수. 주소값은 %p로 표현해야함
 	printf("ptr안에 들어있는 값 %p\n", ptr);
+	// 포인터 변수도 별도의 메모리 주소(공간)에 할당되있다.
 	printf("ptr안에 메모리 주소값 %p\n", &ptr);
 }
