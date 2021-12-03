d6e9694571d7508467b7e669bd0612b46f6fc2a6
cuteNK <80873614+cuteNK@users.noreply.github.com>
2021-12-03 15:37:18 +0900

    구조체 크기 확인

--- 문법설명/15 - 02 구조체.c
+++ 문법설명/15 - 02 구조체.c
@@ -13,4 +13,9 @@ void main(void) {
 	struct point p2;	//struct 안쓰면 에러남
 	p2.x = 20;
 	p2.y = 40;
+
+	printf("(%d, %d) \n", p1.x, p1.y);
+	printf("(%d, %d) \n", p2.x, p2.y);
+	// int변수 2개가 들어있어서 8바이트
+	printf("구조체의 크기는 %d", sizeof(p1));
 }
